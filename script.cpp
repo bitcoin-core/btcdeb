fe0e4db208d6ea1dee16d85e5f7f2ae9a4da58ad
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-12-13 18:01:37 +0900

    Restored accidentally removed copyright statements

--- script.cpp
+++ script.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <script.h>
 #include <utilstrencodings.h>
 
--- script.h
+++ script.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef included_script_h_
 #define included_script_h_
 
