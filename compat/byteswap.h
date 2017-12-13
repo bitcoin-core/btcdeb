464b7e9788836e7b7fcadc644b05a43491aaf0ea
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-12-13 18:37:31 +0900

    Test deploy

--- compat/byteswap.h
+++ compat/byteswap.h
@@ -5,6 +5,10 @@
 #ifndef BITCOIN_COMPAT_BYTESWAP_H
 #define BITCOIN_COMPAT_BYTESWAP_H
 
+#if defined(HAVE_CONFIG_H)
+#include <config/bitcoin-config.h>
+#endif
+
 #include <stdint.h>
 
 #if defined(__APPLE__)
