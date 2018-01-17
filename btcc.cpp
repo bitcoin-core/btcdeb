8b018235a5e0ec2b07003e9c6121ec1de4406e87
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2018-01-17 16:12:59 +0900

    Fix btcc example script

--- btcc.cpp
+++ btcc.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char** argv)
 {
     if (argc < 2) {
         printf("syntax: %s <program>\n", argv[0]);
-        printf("e.g. %s OP_DUP OP_HASH160 62e907b15cbf27d5425399ebf6f0fb50ebb88f18 OP_EQUALVERIFY OP_CHECKSIG\n", argv[0]);
+        printf("e.g. %s OP_DUP OP_HASH160 '[62e907b15cbf27d5425399ebf6f0fb50ebb88f18]' OP_EQUALVERIFY OP_CHECKSIG\n", argv[0]);
         return 1;
     }
     std::vector<Value> result = Value::parse_args(argc, argv, 1);
