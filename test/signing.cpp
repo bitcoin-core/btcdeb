8bde9ccd7f12c24141d21f977536da4164993ac2
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2018-01-26 18:21:30 +0900

    Added more tests

--- test/signing.cpp
+++ test/signing.cpp
@@ -1,1 +1,119 @@
-// ./btcdeb tx=8.947024:010000000001019086ce64fce1bb086395faf6fac37c73f32ba4ea89330432bf8ee8035e9315aa0100000000ffffffff021353b9030000000017a914c3f413d0918853a8e23766678d2e3c2e5c8138bb8725e4973100000000220020701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d040047304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701483045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c016952210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae00000000 52210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae 0x304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701 3045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c01
+#include "catch.hpp"
+
+#include "../instance.h"
+
+/*
+Application version:
+./btcdeb \
+tx=8.947024:010000000001019086ce64fce1bb086395faf6fac37c73f32ba4ea89330432bf8ee8035e9315aa0100000000ffffffff021353b9030000000017a914c3f413d0918853a8e23766678d2e3c2e5c8138bb8725e4973100000000220020701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d040047304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701483045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c016952210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae00000000 \
+52210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae \
+0x \
+304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701 \
+3045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c01
+*/
+#define SCRIPT "52210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae"
+#define STACK1 ""
+#define STACK2 "304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701"
+#define STACK3 "3045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c01"
+#define TXHEX  "010000000001019086ce64fce1bb086395faf6fac37c73f32ba4ea89330432bf8ee8035e9315aa0100000000ffffffff021353b9030000000017a914c3f413d0918853a8e23766678d2e3c2e5c8138bb8725e4973100000000220020701a8d401c84fb13e6baf169d59684e17abd9fa216c8cc5b9fc63d622ff8c58d040047304402207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701483045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c016952210375e00eb72e29da82b89367947f29ef34afb75e8654f6ea368e0acdfd92976b7c2103a1b26313f430c4b15bb1fdce663207659d8cac749a0e53d70eff01874496feff2103c96d495bfdd5ba4145e3e046fee45e84a8a48ad05bd8dbb395c011a32cf9f88053ae00000000"
+#define TXAMT  "8.947024"
+
+TEST_CASE("Segwit Multisig Signing", "[signing-segwit-multisig]") {
+    btc_logf = btc_logf_dummy;
+
+    SECTION("Valid inputs") {
+        Instance instance;
+        instance.parse_transaction(TXAMT ":" TXHEX, true);
+        instance.parse_script(SCRIPT);
+        const char* argv[] = {STACK1, STACK2, STACK3};
+        instance.parse_stack_args(3, argv, 0);
+        instance.setup_environment();
+
+        // Script should have 6 operations and 3 initial items on the stack
+        // The items should be (top-bottom) 0x, 303435..., 304402...
+        REQUIRE(instance.env->stack.size() == 3);
+        REQUIRE(HexStr(instance.stack[0]) == STACK1);
+        REQUIRE(HexStr(instance.stack[1]) == STACK2);
+        REQUIRE(HexStr(instance.stack[2]) == STACK3);
+
+        // Stepping 5 times should place 5 additional items on the stack
+        instance.step(5);
+        REQUIRE(instance.env->stack.size() == 8);
+
+        // Stepping again should put a 01 on the stack
+        REQUIRE(instance.step());
+        REQUIRE(HexStr(instance.env->stack[0]) == "01");
+    }
+
+    SECTION("Invalid signature 1 (stack item 2)") {
+        #define STACK2X "304502207f874ef00f11dcc9a621acad9354f3fca1bf90c43878f607b7e2d358088487e7022052a01b47b8eef5e1c96a6affdc3dac46fdc11b60612464dc8c5921a852090d2701"
+        Instance instance;
+        instance.parse_transaction(TXAMT ":" TXHEX, true);
+        instance.parse_script(SCRIPT);
+        const char* argv[] = {STACK1, STACK2X, STACK3};
+        instance.parse_stack_args(3, argv, 0);
+        instance.setup_environment();
+
+        // Script should have 6 operations and 3 initial items on the stack
+        // The items should be (bottom-top) 0x, 303435..., 304402...
+        REQUIRE(instance.env->stack.size() == 3);
+        REQUIRE(HexStr(instance.stack[0]) == STACK1);
+        REQUIRE(HexStr(instance.stack[1]) == STACK2X);
+        REQUIRE(HexStr(instance.stack[2]) == STACK3);
+
+        // Stepping 5 times should place 5 additional items on the stack
+        instance.step(5);
+        REQUIRE(instance.env->stack.size() == 8);
+
+        // Stepping again should cause a failure
+        REQUIRE(!instance.step());
+    }
+
+    SECTION("Invalid signature 2 (stack item 3)") {
+        #define STACK3X "3045022100c56ab2abb17fdf565417228763bc9f2940a6465042fd62fbd9f4c7406345d7f702201cb1a56b45181f8347713627b325ec5df48fc1aee6bdaf937cbb804d7409b10c00"
+        Instance instance;
+        instance.parse_transaction(TXAMT ":" TXHEX, true);
+        instance.parse_script(SCRIPT);
+        const char* argv[] = {STACK1, STACK2, STACK3X};
+        instance.parse_stack_args(3, argv, 0);
+        instance.setup_environment();
+
+        // Script should have 6 operations and 3 initial items on the stack
+        // The items should be (bottom-top) 0x, 303435..., 304402...
+        REQUIRE(instance.env->stack.size() == 3);
+        REQUIRE(HexStr(instance.stack[0]) == STACK1);
+        REQUIRE(HexStr(instance.stack[1]) == STACK2);
+        REQUIRE(HexStr(instance.stack[2]) == STACK3X);
+
+        // Stepping 5 times should place 5 additional items on the stack
+        instance.step(5);
+        REQUIRE(instance.env->stack.size() == 8);
+
+        // Stepping again should cause a failure
+        REQUIRE(!instance.step());
+    }
+
+    SECTION("Invalid amount") {
+        #define TXAMTX "8.947025"
+        Instance instance;
+        instance.parse_transaction(TXAMTX ":" TXHEX, true);
+        instance.parse_script(SCRIPT);
+        const char* argv[] = {STACK1, STACK2, STACK3};
+        instance.parse_stack_args(3, argv, 0);
+        instance.setup_environment();
+
+        // Script should have 6 operations and 3 initial items on the stack
+        // The items should be (bottom-top) 0x, 303435..., 304402...
+        REQUIRE(instance.env->stack.size() == 3);
+        REQUIRE(HexStr(instance.stack[0]) == STACK1);
+        REQUIRE(HexStr(instance.stack[1]) == STACK2);
+        REQUIRE(HexStr(instance.stack[2]) == STACK3);
+
+        // Stepping 5 times should place 5 additional items on the stack
+        instance.step(5);
+        REQUIRE(instance.env->stack.size() == 8);
+
+        // Stepping again should cause a failure
+        REQUIRE(!instance.step());
+    }
+}
--- test/value.cpp
+++ test/value.cpp
@@ -3,8 +3,63 @@
 #include "../value.h"
 #include "../script.h"
 
+TEST_CASE("Conversions", "[conversions]") {
+    SECTION("Int to hex") {
+        REQUIRE(Value("144").hex_str() == "9000");
+        REQUIRE(Value("123").hex_str() == "7b");
+        REQUIRE(Value("43981").hex_str() == "cdab00");
+        REQUIRE(Value("2635706").hex_str() == "ba3728");
+    }
+
+    SECTION("Hex to int") {
+        REQUIRE(Value("0x9000").int_value() == 144);
+        REQUIRE(Value("7b").int_value() == 123);
+        REQUIRE(Value("cdab00").int_value() == 43981);
+        REQUIRE(Value("ba3728").int_value() == 2635706);
+    }
+
+    SECTION("Back and forth") {
+        for (int64_t i = 0; i < 0x10000000; i += 123 + (i / 3)) {
+            std::vector<uint8_t> data;
+            CScript s;
+            s << i;
+            CScript::const_iterator pc = s.begin();
+            opcodetype opcode;
+            REQUIRE(s.GetOp(pc, opcode, data));
+            // printf("%9lld: %-8s | %s\n", i, Value(i).hex_str().c_str(), HexStr(data).c_str());
+            REQUIRE(Value(i).hex_str() == HexStr(data));
+            REQUIRE(Value((std::string("0x") + HexStr(data)).c_str()).int_value() == i);
+        }
+    }
+}
+
 TEST_CASE("Single entry values are interpreted correctly", "[single-entry-values]") {
     VALUE_WARN = false;
+
+    SECTION("Integer value is an integer value") {
+        Value intv("123");
+        REQUIRE(intv.type == Value::T_INT);
+        REQUIRE(intv.int64 == 123);
+    }
+
+    SECTION("String value is a string") {
+        Value strv("hello");
+        REQUIRE(strv.type == Value::T_STRING);
+        REQUIRE(strv.str == "hello");
+    }
+
+    SECTION("Hex value is data") {
+        Value hexv("0123456789abcdef");
+        REQUIRE(hexv.type == Value::T_DATA);
+        REQUIRE(hexv.hex_str() == "0123456789abcdef");
+    }
+
+    SECTION("Opcode value is opcode") {
+        Value opcv("OP_CHECKSEQUENCEVERIFY");
+        REQUIRE(opcv.type == Value::T_OPCODE);
+        REQUIRE(opcv.opcode == OP_CHECKSEQUENCEVERIFY);
+        REQUIRE(opcv.hex_str() == strprintf("%02x", OP_CHECKSEQUENCEVERIFY));
+    }
 
     SECTION("0x translates to minimal 0-length data") {
         Value minimal0("0x");
@@ -16,27 +71,26 @@ TEST_CASE("Single entry values are interpreted correctly", "[single-entry-values
     SECTION("nonambiguous integers are treated as numbers") {
         Value nonambiguousnum("144");
         REQUIRE(nonambiguousnum.type == Value::T_INT);
-        REQUIRE(nonambiguousnum.i == 144);
-        REQUIRE(nonambiguousnum.hex_str() == "90");
+        REQUIRE(nonambiguousnum.int64 == 144);
+        REQUIRE(nonambiguousnum.hex_str() == "9000");
     }
 
     SECTION("ambiguous integers are also treated as numbers") {
         Value ambiguousnum("1234");
         REQUIRE(ambiguousnum.type == Value::T_INT);
-        REQUIRE(ambiguousnum.i == 1234);
+        REQUIRE(ambiguousnum.int64 == 1234);
     }
 
     SECTION("OP_1 translates to the opcode, not to 0x01") {
         Value opcodev("OP_1");
-        REQUIRE(opcodev.type == Value::T_DATA);
-        REQUIRE(opcodev.data.size() == 1);
+        REQUIRE(opcodev.type == Value::T_OPCODE);
         REQUIRE(opcodev.int_value() == OP_1);
     }
 
     SECTION("1 translates to the integer value, not to OP_1") {
         Value opcodeish("1");
         REQUIRE(opcodeish.type == Value::T_INT);
-        REQUIRE(opcodeish.i == 1);
+        REQUIRE(opcodeish.int64 == 1);
     }
 
     SECTION("123a translates to the hexadecimal value 0x123a") {
@@ -52,11 +106,11 @@ TEST_CASE("Script or not to script", "[script-notscript]") {
     SECTION("515293 becomes the number, not the script") {
         Value notascript("515293");
         REQUIRE(notascript.type == Value::T_INT);
-        REQUIRE(notascript.i == 515293);
+        REQUIRE(notascript.int64 == 515293);
     }
 
     SECTION("515293 becomes the script OP_1 OP_2 OP_ADD if non_numeric is true") {
-        Value ascript("515293", 0, false, false, true);
+        Value ascript("515293", 0, true);
         REQUIRE(ascript.type == Value::T_DATA);
         REQUIRE(ascript.data.size() == 3);
         REQUIRE(ascript.data[0] == OP_1);
@@ -87,8 +141,98 @@ TEST_CASE("Bracketed values", "[bracketed-values]") {
 
 TEST_CASE("Single argument args parsing", "[single-arg-parsing]") {
     SECTION("Fall through of args '1234' becomes the integer value 1234") {
-        Value fallthrough = Value(Value::parse_args("1234", 4), true);
+        Value fallthrough = Value(Value::parse_args("1234"), true);
         REQUIRE(fallthrough.type == Value::T_INT);
-        REQUIRE(fallthrough.i == 1234);
+        REQUIRE(fallthrough.int64 == 1234);
+    }
+}
+
+TEST_CASE("Input format discrepancies (Issue #4)", "[input-format-discrepancies]") {
+    SECTION("OP_ADD 99 OP_EQUAL") {
+        auto withbraces = Value::parse_args("[OP_ADD 99 OP_EQUAL]");
+        auto withoutbcs = Value::parse_args("OP_ADD 99 OP_EQUAL");
+
+        // should have 1 entry, should be data
+        REQUIRE(withbraces.size() == 1);
+        REQUIRE(withbraces[0].type == Value::T_DATA);
+        // should be a push of the given data
+        REQUIRE(withbraces[0].data_value().size() == 4);
+        REQUIRE(withbraces[0].data[0] == OP_ADD);
+        REQUIRE(withbraces[0].data[1] == 1);
+        REQUIRE(withbraces[0].data[2] == 99);
+        REQUIRE(withbraces[0].data[3] == OP_EQUAL);
+
+        // should have 3 entries
+        REQUIRE(withoutbcs.size() == 3);
+        // each entry should be of type data
+        REQUIRE(withoutbcs[0].type == Value::T_OPCODE);
+        REQUIRE(withoutbcs[1].type == Value::T_INT);
+        REQUIRE(withoutbcs[2].type == Value::T_OPCODE);
+        // should be the given data
+        REQUIRE(withoutbcs[0].opcode == OP_ADD);
+        REQUIRE(withoutbcs[1].int64 == 99);
+        REQUIRE(withoutbcs[2].opcode == OP_EQUAL);
+
+        std::string withser = Value::serialize(withbraces);
+        std::string without = Value::serialize(withoutbcs);
+        // with braces should be pushing the content, so it should be prepended with the size of without
+        REQUIRE(withser == Value(int64_t(without.length()>>1)).hex_str() + without);
+    }
+}
+
+TEST_CASE("Data push not larger than necessary", "[data-push-minimal]") {
+    VALUE_WARN = false;
+    char buf[30];
+    for (int i = 0; i < 256; i++) {
+        sprintf(buf, "[%d OP_EQUAL]", i);
+        Value x(buf);
+        REQUIRE(x.type == Value::T_DATA);
+        CScript s;
+        s << i;
+        s << OP_EQUAL;
+        REQUIRE(x.hex_str() == HexStr(s));
+    }
+    for (int64_t i = 257; i < 0x10000000; i = i * 2 - (i >> 5)) {
+        sprintf(buf, "[0x%s OP_EQUAL]", Value(i).hex_str().c_str());
+        Value x(buf);
+        REQUIRE(x.type == Value::T_DATA);
+        CScript s;
+        s << i;
+        s << OP_EQUAL;
+        // printf("%s -> %s / %s\n", buf, x.hex_str().c_str(), HexStr(s).c_str());
+        REQUIRE(x.hex_str() == HexStr(s));
+    }
+}
+
+TEST_CASE("Brace insanity", "[brace-insanity]") {
+    VALUE_WARN = false;
+    SECTION("Double-brace") {
+        const char* script = "93016387";
+        Value xnfallthru(Value::parse_args("[[OP_ADD 99 OP_EQUAL]]"));
+        Value xyfallthru(Value::parse_args("[[OP_ADD 99 OP_EQUAL]]"), true);
+        // xnfallthru is a push of the script [OP_ADD 99 OP_EQUAL], i.e. 0493016387
+        REQUIRE(xnfallthru.hex_str() == std::string("0504") + script);
+    }
+}
+
+TEST_CASE("MAST article examples", "[mast-article]") {
+    SECTION("[01 OP_EQUAL]") {
+        Value x("[01 OP_EQUAL]");
+        REQUIRE(x.hex_str() == "5187");
+    }
+
+    SECTION("OP_FROMALTSTACK OP_1 OP_EQUAL") {
+        Value x("[OP_FROMALTSTACK OP_1 OP_EQUAL]");
+        REQUIRE(x.hex_str() == "6c5187");
+    }
+
+    SECTION("TOALTSTACK 9e2") {
+        Value x("[TOALTSTACK 9e2232a0e2a41073464bdd218fa4ae9221b20ce93af704dceb0db1a0aa253fed OP_2 MERKLEBRANCHVERIFY 2DROP DROP]");
+        REQUIRE(x.hex_str() == "6b209e2232a0e2a41073464bdd218fa4ae9221b20ce93af704dceb0db1a0aa253fed52b36d75");
+    }
+
+    SECTION("1-2 Lightning Network Example") {
+        Value x("[OP_IF 144 OP_CHECKSEQUENCEVERIFY OP_DROP 01 OP_ELSE 02 OP_ENDIF OP_EQUAL]");
+        REQUIRE(x.hex_str() == "63029000b2755167526887");
     }
 }
