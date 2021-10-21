650f5863a77db39ab97c89346111bd3f177606db
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2021-10-21 12:56:32 +0900

    refactor: replace messy tf function stuff with structs

--- functions.cpp
+++ functions.cpp
@@ -226,85 +226,7 @@ int fn_vfexec(const char*) {
     return print_bool_stack(env->vfExec);
 }
 
-static const char* tfs[] = {
-    "echo",
-    "hex",
-    "int",
-    "reverse",
-    "sha256",
-    "ripemd160",
-    "hash256",
-    "hash160",
-    "base58chk-encode",
-    "base58chk-decode",
-    "bech32-encode",
-    "bech32-decode",
-    "verify-sig",
-    "compact-verify-sig",
-    "combine-pubkeys",
-    "tweak-pubkey",
-    "pubkey-to-xpubkey",
-    "addr-to-scriptpubkey",
-    "scriptpubkey-to-addr",
-    "add",
-    "sub",
-    "jacobi-symbol",
-    "tagged-hash",
-    "taproot-tweak-pubkey",
-    "prefix-compact-size",
-#ifdef ENABLE_DANGEROUS
-    "taproot-tweak-seckey",
-    "encode-wif",
-    "decode-wif",
-    "sign",
-    "compact-sign",
-    "get-pubkey",
-    "get-xpubkey",
-    "combine-privkeys",
-    "multiply-privkeys",
-#endif // ENABLE_DANGEROUS
-    nullptr
-};
-
-static const char* tfsh[] = {
-    "[*]       show as-is serialized value",
-    "[*]       convert into a hex string",
-    "[arg]     convert into an integer",
-    "[arg]     reverse the value according to the type",
-    "[message] perform SHA256",
-    "[message] perform RIPEMD160",
-    "[message] perform HASH256 (SHA256(SHA256(message))",
-    "[message] perform HASH160 (RIPEMD160(SHA256(message))",
-    "[pubkey]  encode [pubkey] using base58 encoding (with checksum)",
-    "[string]  decode [string] into a pubkey using base58 encoding (with checksum)",
-    "[pubkey]  encode [pubkey] using bech32 encoding",
-    "[string]  decode [string] into a pubkey using bech32 encoding",
-    "[sighash] [pubkey] [signature] verify the given signature for the given sighash and pubkey (der)",
-    "[sighash] [pubkey] [signature] verify the given signature for the given sighash and pubkey (compact)",
-    "[pubkey1] [pubkey2] combine the two pubkeys into one pubkey",
-    "[value] [pubkey] multiply the pubkey with the given 32 byte value",
-    "[pubkey] convert the given pubkey into an x-only pubkey, as those used in taproot/tapscript",
-    "[address] convert a base58 encoded address into its corresponding scriptPubKey",
-    "[script]  convert a scriptPubKey into its corresponding base58 encoded address",
-    "[value1] [value2] add two values together",
-    "[value1] [value2] subtract value2 from value1",
-    "[n] ([k]) calculate the Jacobi symbol for n modulo k, where k defaults to the secp256k1 field size",
-    "[tag] [message] generate the [tag]ged hash of [message]",
-    "[pubkey] [tweak] tweak the pubkey with the tweak",
-    "[value] prefix [value] with its compact size encoded byte length",
-#ifdef ENABLE_DANGEROUS
-    "[privkey] [tweak] tweak the given private key with the tweak",
-    "[privkey] encode [privkey] using the Wallet Import Format",
-    "[string]  decode [string] into a private key using the Wallet Import Format",
-    "[sighash] [privkey] generate a signature for the given message (sighash) using the given private key (der)",
-    "[sighash] [privkey] generate a signature for the given message (sighash) using the given private key (compact)",
-    "[privkey] get the public key corresponding to the given private key",
-    "[privkey] get the x-only public key corresponding to the given private key",
-    "[privkey1] [privkey2] combine the two private keys into one private key",
-    "[privkey1] [privkey2] multiply a privkey with another",
-#endif // ENABLE_DANGEROUS
-    nullptr
-};
+typedef int (*btcdeb_tfun) (Value&&);
 
 int _e_echo(Value&& pv)       { pv.println(); return 0; }
 int _e_hex(Value&& pv)        { printf("%s\n", pv.hex_str().c_str()); return 0; }
@@ -343,45 +265,53 @@ int _e_combine_privkeys(Value&& pv) { kerl_set_sensitive(true); pv.do_combine_pr
 int _e_mul_privkeys(Value&& pv)  { kerl_set_sensitive(true); pv.do_multiply_privkeys(); pv.println(); return 0; }
 #endif // ENABLE_DANGEROUS
 
-typedef int (*btcdeb_tfun) (Value&&);
-static const btcdeb_tfun tffp[] = {
-    _e_echo,
-    _e_hex,
-    _e_int,
-    _e_reverse,
-    _e_sha256,
-    _e_ripemd160,
-    _e_hash256,
-    _e_hash160,
-    _e_b58ce,
-    _e_b58cd,
-    _e_b32e,
-    _e_b32d,
-    _e_verify_sig,
-    _e_verify_sig_compact,
-    _e_combine_pubkeys,
-    _e_tweak_pubkey,
-    _e_pubkey_to_xpubkey,
-    _e_addr_to_spk,
-    _e_spk_to_addr,
-    _e_add,
-    _e_sub,
-    _e_jacobi_sym,
-    _e_tagged_hash,
-    _e_taproot_tweak_pubkey,
-    _e_prefix_compact_size,
+struct tf_t {
+    const char* name;
+    const char* inl; // when used in fn() form
+    const char* help;
+    btcdeb_tfun fun;
+};
+
+static const tf_t tfs[] = {
+    #define TFN(help, name, fun) { name, #fun, help, _e_##fun }
+    #define TF(help, name) TFN(help, #name, name)
+    TFN("[address] convert a base58 encoded address into its corresponding scriptPubKey", "addr-to-scriptpubkey", addr_to_spk),
+    TF ("[value1] [value2] add two values together", add),
+    TFN("[string]  decode [string] into a pubkey using bech32 encoding", "bech32-decode", b32d),
+    TFN("[pubkey]  encode [pubkey] using bech32 encoding", "bech32-encode", b32e),
+    TFN("[string]  decode [string] into a pubkey using base58 encoding (with checksum)", "base58chk-decode", b58cd),
+    TFN("[pubkey]  encode [pubkey] using base58 encoding (with checksum)", "base58chk-encode", b58ce),
+    TFN("[pubkey1] [pubkey2] combine the two pubkeys into one pubkey", "combine-pubkeys", combine_pubkeys),
+    TF ("[*]       show as-is serialized value", echo),
+    TF ("[message] perform HASH160 (RIPEMD160(SHA256(message))", hash160),
+    TF ("[message] perform HASH256 (SHA256(SHA256(message))", hash256),
+    TF ("[*]       convert into a hex string", hex),
+    TF ("[arg]     convert into an integer", int),
+    TFN("[n] ([k]) calculate the Jacobi symbol for n modulo k, where k defaults to the secp256k1 field size", "jacobi-symbol", jacobi_sym),
+    TFN("[value] prefix [value] with its compact size encoded byte length", "prefix-compact-size", prefix_compact_size),
+    TFN("[pubkey] convert the given pubkey into an x-only pubkey, as those used in taproot/tapscript", "pubkey-to-xpubkey", pubkey_to_xpubkey),
+    TF ("[arg]     reverse the value according to the type", reverse),
+    TF ("[message] perform RIPEMD160", ripemd160),
+    TF ("[message] perform SHA256", sha256),
+    TFN("[script]  convert a scriptPubKey into its corresponding base58 encoded address", "scriptpubkey-to-addr", spk_to_addr),
+    TF ("[value1] [value2] subtract value2 from value1", sub),
+    TFN("[tag] [message] generate the [tag]ged hash of [message]", "tagged-hash", tagged_hash),
+    TFN("[pubkey] [tweak] tweak the pubkey with the tweak", "taproot-tweak-pubkey", taproot_tweak_pubkey),
+    TFN("[value] [pubkey] multiply the pubkey with the given 32 byte value", "tweak-pubkey", tweak_pubkey),
+    TFN("[sighash] [pubkey] [signature] verify the given signature for the given sighash and pubkey (der)", "verify-sig", verify_sig),
+    TFN("[sighash] [pubkey] [signature] verify the given signature for the given sighash and pubkey (compact)", "compact-verify-sig", verify_sig_compact),
 #ifdef ENABLE_DANGEROUS
-    _e_taproot_tweak_seckey,
-    _e_encode_wif,
-    _e_decode_wif,
-    _e_sign,
-    _e_sign_compact,
-    _e_get_pubkey,
-    _e_get_xpubkey,
-    _e_combine_privkeys,
-    _e_mul_privkeys,
+    TFN("[privkey1] [privkey2] combine the two private keys into one private key", "combine-privkeys", combine_privkeys),
+    TFN("[string]  decode [string] into a private key using the Wallet Import Format", "decode-wif", decode_wif),
+    TFN("[privkey] encode [privkey] using the Wallet Import Format", "encode-wif", encode_wif),
+    TFN("[privkey] get the public key corresponding to the given private key", "get-pubkey", get_pubkey),
+    TFN("[privkey] get the x-only public key corresponding to the given private key", "get-xpubkey", get_xpubkey),
+    TFN("[privkey1] [privkey2] multiply a privkey with another", "multiply-privkeys", mul_privkeys),
+    TF ("[sighash] [privkey] generate a signature for the given message (sighash) using the given private key (der)", sign),
+    TFN("[sighash] [privkey] generate a signature for the given message (sighash) using the given private key (compact)", "compact-sign", sign_compact),
+    TFN("[privkey] [tweak] tweak the given private key with the tweak", "taproot-tweak-seckey", taproot_tweak_seckey),
 #endif // ENABLE_DANGEROUS
-    nullptr
+    { "", "", nullptr }
 };
 
 int fn_tf(const char* arg) {
@@ -395,40 +325,36 @@ int fn_tf(const char* arg) {
         printf("syntax: tf <command> [<param1> [...]]\n");
         printf("transform a value using some function\n");
         printf("available functions are (tf -h for details):");
-        for (int i = 0; tfs[i]; i++) {
-            printf(" %s", tfs[i]);
+        for (int i = 0; tfs[i].fun; ++i) {
+            printf(" %s", tfs[i].name);
         }
         printf("\nexample: tf hex 35        (output: 0x23)\n");
         return 0;
     }
     if (argc == 1 && !strcmp("-h", argv[0])) {
-        for (int i = 0; tfs[i]; i++) {
-            printf("%-16s %s\n", tfs[i], tfsh[i]);
+        for (int i = 0; tfs[i].fun; i++) {
+            printf("%-16s %s\n", tfs[i].name, tfs[i].help);
         }
-        printf(
-            "\nThe inline operators have slightly different names; they are called: echo, hex, int, reverse, sha256"
-            ", ripemd160, hash256, hash160, base58chkenc, base58chkdec, bech32enc, bech32dec, verify_sig"
-            ", combine_pubkeys, tweak_pubkey, pubkey_to_xpubkey, addr_to_spk, spk_to_addr, add, sub, jacobi, tagged_hash, taproot_tweak_pubkey, prefix_compact_size"
-#ifdef ENABLE_DANGEROUS
-            ", taproot_tweak_seckey, combine_privkeys, multiply_privkeys, nnegate_privkey, encode_wif, decode_wif, sign, get_pubkey, get_xpubkey"
-#endif // ENABLE_DANGEROUS
-            "\n"
-        );
+        printf("\nThe inline operators have slightly different names; they are called:");
+        for (int i = 0; tfs[i].fun; i++) {
+            printf(" %s", tfs[i].inl);
+        }
+        printf("\n");
         return 0;
     }
     int i;
-    for (i = 0; tfs[i] && strcmp(tfs[i], argv[0]); i++);
-    if (!tfs[i]) {
+    for (i = 0; tfs[i].fun && strcmp(tfs[i].name, argv[0]); i++);
+    if (!tfs[i].fun) {
         printf("unknown function: %s\n", argv[0]);
         return -1;
     }
     if (argc == 1) {
-        puts(tfsh[i]);
+        puts(tfs[i].help);
         return 0;
     }
     int rv;
     try {
-        rv = tffp[i](Value(Value::parse_args(argc, (const char**)argv, 1), true));
+        rv = tfs[i].fun(Value(Value::parse_args(argc, (const char**)argv, 1), true));
     } catch (std::exception const& ex) {
         fprintf(stderr, "exception: %s\n", ex.what());
         rv = -1;
@@ -450,8 +376,8 @@ char* compl_tf(const char* text, int continued) {
     }
 
     /* Return the next name which partially matches from the names list. */
-    while (tfs[++list_index]) {
-        name = tfs[list_index];
+    while (tfs[++list_index].fun) {
+        name = tfs[list_index].name;
 
         if (strncasecmp(name, text, len) == 0)
             return strdup(name);
